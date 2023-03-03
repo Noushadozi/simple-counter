215f9721d145f47d58649c79ee6459f62937ed5f
Noushadozi <noushadozi333@gmail.com>
2023-03-03 15:23:51 +0600

    first commit

---
+++ count.js
@@ -0,0 +1,67 @@
+let value = 0;
+document.getElementById('blue-btn').addEventListener('click', function(){
+    value += 1;
+    document.getElementById('counter').innerText = value;
+});
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+// function reduceNumber(){
+//         value -= 1;
+//         document.getElementById('counter').innerText = value;
+// };
+
+document.getElementById('btn-success').addEventListener('click', function(){
+    value -= 1;
+    document.getElementById('counter').innerText = value;
+});
+
+
